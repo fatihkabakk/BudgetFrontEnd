a84302e65c8cde2fa8f43a6d49e4ac4add694b5d
Fatih Kabak <fatihkabak39@hotmail.com>
2022-04-08 15:22:25 +0300

    Add Router

---
+++ src/layouts/ComponentRouter.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import { Routes, Route } from 'react-router';
+
+export default function Router() {
+	return (
+		<div>
+			<ToastContainer position='bottom-right' />
+			<Routes>
+				{/* <Route exact path='/login' element={<Login />} /> */}
+				{/* <Route exact path='/' component={Index} />
+			<Route exact path='/dashboard' component={Dashboard} />
+			<Route path='/products/:name' component={} />
+			<Routes exact path='/profile' component={Profile} /> */}
+			</Routes>
+		</div>
+	);
+}
