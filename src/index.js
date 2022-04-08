aa22471bbd7c357108094b7f86b07a09128ba5f7
Fatih Kabak <fatihkabak39@hotmail.com>
2022-04-08 15:23:44 +0300

    Add router into index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 let root = createRoot(document.getElementById('root'));
 
 root.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 );
 
