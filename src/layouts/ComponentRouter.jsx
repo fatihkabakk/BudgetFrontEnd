d96ec146695cec548a226bce8a95df7877c39b0d
Fatih Kabak <fatihkabak39@hotmail.com>
2022-04-08 15:24:58 +0300

    Add Login router

--- src/layouts/ComponentRouter.jsx
+++ src/layouts/ComponentRouter.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import { Routes, Route } from 'react-router';
+import Login from './Login';
 
 export default function Router() {
 	return (
 		<div>
 			<ToastContainer position='bottom-right' />
 			<Routes>
-				{/* <Route exact path='/login' element={<Login />} /> */}
+				<Route exact path='/login' element={<Login />} />
 				{/* <Route exact path='/' component={Index} />
 			<Route exact path='/dashboard' component={Dashboard} />
 			<Route path='/products/:name' component={} />
