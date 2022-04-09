90d6333125c99bdea400cc9f3de5597802285ab1
Fatih Kabak <fatihkabak39@hotmail.com>
2022-04-09 04:02:47 +0300

    Register SignUp route in router

--- src/layouts/ComponentRouter.jsx
+++ src/layouts/ComponentRouter.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { ToastContainer } from 'react-toastify';
-import { Routes, Route } from 'react-router';
 import Login from './Login';
+import Register from './Register';
+import { Routes, Route } from 'react-router';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 export default function Router() {
 	return (
 		<div>
-			<ToastContainer position='bottom-right' />
+			<ToastContainer
+				position='bottom-right'
+				autoClose={2000}
+				closeOnClick
+				rtl={false}
+				draggable
+				pauseOnHover
+			/>
 			<Routes>
 				<Route exact path='/login' element={<Login />} />
+				<Route exact path='/register' element={<Register />} />
 				{/* <Route exact path='/' component={Index} />
 			<Route exact path='/dashboard' component={Dashboard} />
 			<Route path='/products/:name' component={} />
